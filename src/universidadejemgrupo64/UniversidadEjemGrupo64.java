d8dd5ade032a4f7d46437c9c59e0b6bce9db049c
Jesy Gomez <julianycia.m@gmail.com>
2023-09-14 18:19:22 -0300

    ponele que dejamos

--- src/universidadejemgrupo64/UniversidadEjemGrupo64.java
+++ src/universidadejemgrupo64/UniversidadEjemGrupo64.java
@@ -17,11 +17,6 @@ public class UniversidadEjemGrupo64 {
 
         Connection con=Conexion.getConexion();
         
-        
-        ///PROBANDO DEEEE NUEVOOOOOOOO!!!!!!!!
-        //// GRACIAS ///
-        
-        
         //Alumno Alejandro=new Alumno(3,30597421,"Lopez","Lionel",LocalDate.of(1980, 5, 20),true);
         //AlumnoData alu = new AlumnoData();
         //alu.guardarAlumno(Alejandro);
